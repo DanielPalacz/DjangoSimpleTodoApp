44d1b28b8942f8e671c2609b73ce9560ec303170
Daniel <daniel.palacz@gmail.com>
2024-07-21 15:57:15 +0200

    adds custom form feature

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,12 +9,32 @@ def index(request, pk):
     # item = todolist.todoitem_set.get(id=1)
     # return HttpResponse(f"<h1>* Hello from v1 [pk={pk}]</h1><h1>* Todolist name: {todolist.name!r} {item.text}</h1>")
 
+    if request.method == "POST":
+        print(request.POST)
+        if request.POST.get("save"):
+            for item in todolist.todoitem_set.all():
+                if request.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif request.POST.get("new"):
+            txt = request.POST.get("new")
+            if len(txt) > 2:
+                todolist.todoitem_set.create(text=txt, complete=False)
+            else:
+                print("invalid input")
+
+
+
     return render(request, "main/list.html", {"todolist": todolist})
 
 
 def home(request):
-    # return HttpResponse("<h1>Hello from home page.</h1>")
-    return render(request, "main/home.html", {"name": "test"})
+    todolists = TodoList.objects.all()
+    return render(request, "main/home.html", {"todolists": todolists})
 
 
 def create(request):
